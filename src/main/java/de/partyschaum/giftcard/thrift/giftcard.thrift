79c135171343b89f7478a5f563fe95021dcc89fd
Hauke Stange <partyschaum@posteo.de>
2018-10-05 21:47:16 +0200

    Adds GiftCard Thrift IDL

--- src/main/java/de/partyschaum/giftcard/thrift/giftcard.thrift
+++ src/main/java/de/partyschaum/giftcard/thrift/giftcard.thrift
@@ -1,16 +1,22 @@
+namespace java de.partyschaum.giftcard.thrift
+
 typedef string GiftCardId
 
 struct IssueGiftCardRequest {
-    1: optional GiftCardId id;
-    2: optional i32 amount;
+    1: optional GiftCardId id
+    2: optional i32 amount
 }
 
 struct RedeemGiftCardRequest {
-    1: optional GiftCardId id;
-    2: optional i32 amount;
+    1: optional GiftCardId id
+    2: optional i32 amount
+}
+
+exception GiftCardException {
+    1: optional string message
 }
 
 service GiftCard {
-    void issueGiftCard(1: IssueGiftCardRequest request)
-    void redeemGiftCard(1: RedeemGiftCardRequest request)
+    void issueGiftCard(1: IssueGiftCardRequest request) throws (1: GiftCardException error)
+    void redeemGiftCard(1: RedeemGiftCardRequest request) throws (1: GiftCardException error)
 }
